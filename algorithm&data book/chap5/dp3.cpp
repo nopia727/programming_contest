540102b18d59a51726dd5a1a8aee8a69f13d4161
nopia727 <nopia20000727@gmail.com>
2023-03-29 22:04:43 +0900

    add

--- algorithm&data book/chap5/dp3.cpp
+++ algorithm&data book/chap5/dp3.cpp
@@ -20,7 +20,7 @@ int main()
     vector<vector<long long>> c(n + 1, vector<long long>(n + 1));
     for (int i = 0; i < n + 1; i++)
     {
-        for (int j = 0; j < n + 1; j++)
+        for (int j = 0; j < n; j++)
         {
             cin >> c[i][j];
         }
